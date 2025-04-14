692206ef68f08d849157b326ef69effa5b4fa00a
Geovane Ribeiro Rosa <rrosageovane@gmail.com>
2025-04-14 11:49:08 -0300

    Update Gerador_de-Senha.py

--- Gerador_de-Senha.py
+++ Gerador_de-Senha.py
@@ -3,8 +3,11 @@ def gerador_senha(x):
     lista = list(x)
     random.shuffle(lista)
     size = int(input("Digite o tamanho da senha (MÁXIMO 67 CARACTERES): "))
-    for c in range(size):
-        print(lista[c], end="")
+    if size<5:
+        print("Senha Curta!! Mínimo 5 caracteres")
+    else:
+        for c in range(size):
+            print(lista[c], end="")
 
 options = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ123456789!@#$%&"
 
