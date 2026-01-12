3989a56b616cc1c53cc7c4bf10359e89e8e2f991
Geovane Ribeiro Rosa <rrosageovane@gmail.com>
2026-01-12 10:13:08 -0300

    Rename file from Soma_de_dois_números.py to Sum_of_Two_Numbers.py

--- Soma_de_dois_números.py
+++
@@ -1,5 +0,0 @@
-num1=float(input("Digite um número: "))
-num2=float(input("Digite outro número: "))
-soma=num1+num2
-
-print(f"A soma desses números é: {soma}")
---
+++ Sum_of_Two_Numbers.py
@@ -0,0 +1,6 @@
+num1 = float(input("Enter a number: "))
+num2 = float(input("Enter another number: "))
+sum_result = num1 + num2
+
+print(f"The sum of these numbers is: {sum_result}")
+
